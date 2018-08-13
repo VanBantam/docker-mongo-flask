6b3172c87b01c855d7e2438d08e8a6a9a6a4e63f
VanBantam <thetopquark@gmail.com>
2018-08-13 10:39:05 -0700

    Initial directory structure as per the tutorial part 1.

---
+++ setup.sh
@@ -0,0 +1,6 @@
+mkdir -p python-flask/modules/app/dist
+mkdir -p python-flask/modules/logger;
+touch python-flask/modules/app/__init__.py;
+touch python-flask/modules/app/dist/404.html;
+touch python-flask/modules/app/dist/index.html;
+touch docker-compose.pro.yml docker-compose.yml Dockerfile index.py output.log requirements.txt;
